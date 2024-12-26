3dd9db190e494ff4757a3cd3f3a9512778f17a9f
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2024-12-26 15:26:43 +0530

    Day 10 works started

---
+++ myMobileApp/components/ContactUs.jsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export default function ContactUs() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+
+  },
+  body: {
+
+  },
+  footer: {
+
+  },
+});
